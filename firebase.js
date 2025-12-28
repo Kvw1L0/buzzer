ec2aa66d3184a1de374eb10ee3b2b36f4b38f900
Camilo Martinez <134472412+Kvw1L0@users.noreply.github.com>
2025-12-28 00:42:52 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDlx7HxHyPNuXxueFyPjeKn84EpFbgke1Y",
     authDomain: "buzzer-67109.firebaseapp.com",
     projectId: "buzzer-67109",
@@ -13,9 +6,5 @@
     messagingSenderId: "925012683826",
     appId: "1:925012683826:web:f200286df975b4969a602b"
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
 
 
