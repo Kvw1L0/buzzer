173d9cce64bcb755e48fe43c0b9a843d60f23624
Camilo Martinez <134472412+Kvw1L0@users.noreply.github.com>
2025-12-28 00:38:36 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,21 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDlx7HxHyPNuXxueFyPjeKn84EpFbgke1Y",
-  authDomain: "buzzer-67109.firebaseapp.com",
-  databaseURL: "https://TU_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "buzzer-67109",
-  storageBucket: "TU_PROJECT.appspot.com",
-  messagingSenderId: "925012683826",
-  appId: "1:925012683826:web:f200286df975b4969a602b"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDlx7HxHyPNuXxueFyPjeKn84EpFbgke1Y",
+    authDomain: "buzzer-67109.firebaseapp.com",
+    projectId: "buzzer-67109",
+    storageBucket: "buzzer-67109.firebasestorage.app",
+    messagingSenderId: "925012683826",
+    appId: "1:925012683826:web:f200286df975b4969a602b"
+  };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
 
 
