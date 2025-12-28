bb3f0f7962d4fde7beb575f8103a1b543e4459f6
Camilo Martinez <134472412+Kvw1L0@users.noreply.github.com>
2025-12-28 00:55:56 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,16 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDlx7HxHyPNuXxueFyPjeKn84EpFbgke1Y",
-    authDomain: "buzzer-67109.firebaseapp.com",
-    projectId: "buzzer-67109",
-    storageBucket: "buzzer-67109.firebasestorage.app",
-    messagingSenderId: "925012683826",
-    appId: "1:925012683826:web:f200286df975b4969a602b"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "buzzer-67109.firebaseapp.com",
+  databaseURL: "https://buzzer-67109-default-rtdb.firebaseio.com",
+  projectId: "buzzer-67109",
+  storageBucket: "buzzer-67109.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 
